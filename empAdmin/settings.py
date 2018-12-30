d456da12d8607b6e9ac5ba1cd146c59d05fe4cba
StacyDu <dudinaanastasiia@gmail.com>
2018-12-30 20:02:28 +0100

    include mysql tothe project

--- empAdmin/settings.py
+++ empAdmin/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'empAdmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'contractors',
+        'USER': 'emp',
+        'PASSWORD': 'empadmin2019',
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
